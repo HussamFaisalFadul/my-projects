ade3e107ccbd578c3187fd32519fec024c86e9b2
HussamFaisalFadul <155633149+HussamFaisalFadul@users.noreply.github.com>
2026-04-08 15:13:51 +0300

    Update vite.config.ts

--- store-dashboard/frontend/vite.config.ts
+++ store-dashboard/frontend/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  }
+  build: {
+    rollupOptions: {
+      // هذا السطر سيخبر Vite: "حتى لو وجدت أحداً يطلب lucide، تجاهله ولا توقف الـ Build"
+      external: ['lucide-react'],
+    },
+  },
 })
